67bb48523b4092df8be1af47726707357d94e12d
Sid <siddhaarthabs@gmail.com>
2025-05-20 20:25:17 +0530

    signup route created

--- backend/src/index.js
+++ backend/src/index.js
@@ -5,7 +5,11 @@ dotenv.config();
 import authRoute from './routes/auth.route.js'
 import { connectDB } from './lib/db.js';
 const PORT = process.env.PORT;
+
 const app = express();
+//use this middleware inorder to receive body content when 
+app.use(express.json());
+
 
 app.use("/api/auth",authRoute)
 
