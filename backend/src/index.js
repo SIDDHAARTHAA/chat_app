3e80e04e020283cfee0f32af6fbae857f05d258f
Sid <siddhaarthabs@gmail.com>
2025-06-04 21:33:49 +0530

    some more changes

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,6 +1,7 @@
 import express from 'express'
 import dotenv from "dotenv"
 import cookieParser from 'cookie-parser'
+import cors from 'cors'
 
 dotenv.config();
 import authRoute from './routes/auth.route.js'
@@ -11,6 +12,10 @@ const app = express();
 //use this middleware inorder to receive body content when 
 app.use(express.json());
 app.use(cookieParser())
+app.use(cors({
+    origin:"http:localhost:5173",
+    credentials:true
+}))
 
 
 app.use("/api/auth",authRoute)
--- frontend/src/store/useAuthStore.js
+++ frontend/src/store/useAuthStore.js
@@ -1,7 +1,28 @@
+import axios from 'axios'
 import {create} from 'zustand'
+import { axioisInstance } from '../lib/axios.js'
+
 
 export const useAuthStore = create((set) =>({
     authUser:null,
-    isCheckingAuth:true
-}))
+    isSigningUp: false,
+    isLoginngIn: false,
+    isUpdatingProfile: false,
+    isCheckingAuth:true,
+    checkAuth:  async() =>{
+        try {
+            const res = await axioisInstance.get('/auth/check')
 
+            set({authUser:res.data})
+        } catch (error) {
+            console.error("Error in checkAuth",error)
+            set({authUser:null})
+        }finally{
+            set({isCheckingAuth:null})
+        }
+    },
+    signup: async (data) => {
+
+    }
+
+}))
